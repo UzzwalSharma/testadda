43078ec4b66b473d95fb5b63b0d802d4d95e401e
ujjwal <uzzwal7505@gmail.com>
2026-02-22 18:57:32 +0530

    changes

--- src/Mainpage/Tests/aitest.jsx
+++ src/Mainpage/Tests/aitest.jsx
@@ -29,7 +29,7 @@ function AiTestGenerator() {
     setLoading(true);
 
     try {
-      const response = await fetch("https://localhost:/ai-test-generator", {
+      const response = await fetch("https://tuitionserver.onrender.com/ai-test-generator", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
